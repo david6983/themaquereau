22c512257400d611364e250ed74d71a027c02355
david haioum <dav6983@gmail.com>
2021-01-25 10:41:49 +0100

    add: display a toast on click in each menu item + fix divider color

--- app/src/main/java/fr/isen/david/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/david/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,30 @@ package fr.isen.david.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity_layout)
+
+        // temporary display function to display a toast given a text string
+        val displayToast = { text: CharSequence ->
+            val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        // listen click on every text in the main menu
+        findViewById<TextView>(R.id.homeEntreeButton).setOnClickListener {
+            displayToast("Entrées")
+        }
+        findViewById<TextView>(R.id.homePlatsButton).setOnClickListener {
+            displayToast("Plats")
+        }
+        findViewById<TextView>(R.id.homeDesertsButton).setOnClickListener {
+            displayToast("Deserts")
+        }
     }
 }
