0d89f900e20672466aa0035db284486cd09d4e42
david haioum <dav6983@gmail.com>
2021-01-29 11:43:47 +0100

    add: finalise order button in basket

--- app/src/main/java/fr/isen/david/themaquereau/BasketActivity.kt
+++ app/src/main/java/fr/isen/david/themaquereau/BasketActivity.kt
@@ -3,6 +3,7 @@ package fr.isen.david.themaquereau
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
@@ -24,12 +25,14 @@ class BasketActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val gson = Gson()
+
         // Retrieve the orders from file if exist
         try {
             applicationContext.openFileInput(DishDetailsActivity.ORDER_FILE).use { inputStream ->
                 inputStream.bufferedReader().use {
                     val ordersJsonString = it.readText()
-                    orders = Gson().fromJson(ordersJsonString, Array<Order>::class.java).toMutableList()
+                    orders = gson.fromJson(ordersJsonString, Array<Order>::class.java).toMutableList()
 
                     // Render the orders in the recycle view
                     val rvOrders = binding.orderList
@@ -48,6 +51,13 @@ class BasketActivity : AppCompatActivity() {
             // redirect to the parent activity
             val intent = getParentActivityIntentImpl()
             startActivity(intent)
+        }
+
+        // Listener finalise order
+        binding.finalOrderButton.setOnClickListener {
+            // Convert to JsonArray the orders
+            val finalOrder = gson.toJson(orders)
+            Log.i(TAG, "The final order is $finalOrder")
         }
     }
 
@@ -72,5 +82,9 @@ class BasketActivity : AppCompatActivity() {
             parentIntent.setClass(this, DishDetailsActivity::class.java)
         }
         return parentIntent
+    }
+
+    companion object {
+        val TAG = BasketActivity::class.java.simpleName
     }
 }
