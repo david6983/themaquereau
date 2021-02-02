1fee4b8e5cca3a13f1238aecf9868541e7d14e49
david haioum <dav6983@gmail.com>
2021-02-02 10:13:18 +0100

    add: contct activity + google map location

---
+++ app/src/main/java/fr/isen/david/themaquereau/fragments/MapsFragment.kt
@@ -0,0 +1,38 @@
+package fr.isen.david.themaquereau.fragments
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import fr.isen.david.themaquereau.R
+
+class MapsFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val toulon = LatLng(43.12, 5.92)
+        googleMap.addMarker(MarkerOptions().position(toulon).title("Marqueur sur Toulon"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toulon, 14.0f))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
